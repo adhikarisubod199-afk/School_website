225f0f572aee8c36866f2de0566f01da69594ad5
Subod Adhikari <Adhikarisubod199@gmail.com>
2026-07-15 12:57:51 +0545

    Updated school website

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-
 console.log("Connected");
 
 const buttonlast = document.getElementById("buttonlast");
 
-if (buttonlast) {
-    window.onscroll = () => {
-        buttonlast.style.display = window.scrollY > 400 ? "block" : "none";
-    };
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY); // Check scrolling
 
-    buttonlast.onclick = () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    };
-} else {
-    console.error("Button with id='buttonlast' not found.");
-}
+    if (window.scrollY > 200) {
+        buttonlast.style.display = "flex";
+    } else {
+        buttonlast.style.display = "none";
+    }
+});
+
+buttonlast.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+const preb = document.getElementById("preb");
+preb.addEventListener("click", () => {
+    open("apply.html", "_blank");
+});
+const preb1 = document.getElementById("preb1");
+preb1.addEventListener("click", () => {
+    open("apply.html", "_blank");
+});
+const moreb = document.getElementById("moreb");
+moreb.addEventListener("click", () => {
+    open("more.html", "_blank");
+});
